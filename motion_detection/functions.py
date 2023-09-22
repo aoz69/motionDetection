6af995b7e9663ec1f76bb54bf066e17ee42b4fb0
aoz69 <aozpokharel@gmail.com>
2023-09-22 14:47:19 +0545

    added code need to test

--- motion_detection/functions.py
+++ motion_detection/functions.py
@@ -3,6 +3,14 @@ import threading
 import os
 from tkinter import filedialog
 
+# Define global variables to control video recording.
+is_recording = False
+start_time = 0
+video_writer = None
+
+
+
+
 # Function to perform motion detection from a video source
 def motion_detection(video_source):
     cap = cv2.VideoCapture(video_source)
@@ -22,6 +30,7 @@ def motion_detection(video_source):
 
         if motion_detected:
             save_cropped_image(frame1)
+            # save_frame_to_video(frame1)
 
         show_video_frame(frame1)
 
@@ -98,3 +107,83 @@ def use_video_file():
 # Function to use the webcam for motion detection
 def use_webcam():
     start_motion_detection(0)  # 0 represents the default webcam index
+
+
+
+
+
+
+
+
+
+
+
+# # Function to start video recording.
+# def start_recording(frame):
+#     global is_recording, start_time, video_writer
+
+#     # Define the absolute directory path where the video will be saved.
+#     dir = os.path.abspath("./videos")
+
+#     # Check if the directory exists, and if not, create it.
+#     if not os.path.exists(dir):
+#         os.makedirs(dir)
+
+#     # Define the complete file path including the directory and filename.
+#     filename = f"{dir}/motionVideo.mp4"
+
+#     # Get the frame width and height to set up the video writer.
+#     frame_height, frame_width, _ = frame.shape
+
+#     # Define the codec and create a VideoWriter object.
+#     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+#     video_writer = cv2.VideoWriter(filename, fourcc, 30, (frame_width, frame_height))
+
+#     # Set the start time and recording flag.
+#     start_time = cv2.getTickCount()
+#     is_recording = True
+
+# # Function to stop video recording.
+# def stop_recording():
+#     global is_recording, video_writer
+
+#     # Release the video writer object.
+#     if video_writer is not None:
+#         video_writer.release()
+#         video_writer = None
+
+#     is_recording = False
+
+# # Function to save a frame to the video file.
+# def save_frame_to_video(frame):
+#     global is_recording, video_writer
+
+#     # Write the frame to the video file if recording is active.
+#     if is_recording:
+#         video_writer.write(frame)
+
+# # Function to check if recording duration has reached 2 seconds.
+# def has_recorded_enough():
+#     global is_recording, start_time
+
+#     # Calculate the elapsed time since recording started.
+#     elapsed_time = (cv2.getTickCount() - start_time) / cv2.getTickFrequency()
+
+#     # If 2 seconds have passed, stop recording.
+#     if elapsed_time >= 2:
+#         stop_recording()
+
+# # Function to save a 2-second video clip when motion is detected.
+# def save_video_clip(frame):
+#     global is_recording
+
+#     # Check if recording is active.
+#     if not is_recording:
+#         start_recording(frame)
+
+#     # Save the frame to the video file.
+#     save_frame_to_video(frame)
+
+#     # Check if 2 seconds have passed and stop recording.
+#     has_recorded_enough()
+
